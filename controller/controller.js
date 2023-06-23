eef53429c69fa6b726bcc4b4c5a9d040bc876169
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-23 10:09:38 +0300

    register farmerone router

--- controller/controller.js
+++ controller/controller.js
@@ -1,26 +1,35 @@
 const connection = require("../database");
 
-exports.createTable = (req,res)=>{
-    const createFarmerone = 'CREATE TABLE farmerOne (id int AUTO_INCREMENT, NAME VARCHAR(50),PRIMARY KEY(id))';
-    connection.query(createFarmerone,(error,result)=>{
-        if(error){
-            console.error("creating table", error);
-            return res.status(500).json({ message: "failed to create table" });
-            
-        }
-        return res.status(200).json({ message: "table created" });
-    });
-}
+exports.createTable = (req, res) => {
+  const createFarmerone =
+    "CREATE TABLE farmerOne (id int AUTO_INCREMENT, NAME VARCHAR(50),PRIMARY KEY(id))";
+  connection.query(createFarmerone, (error, result) => {
+    if (error) {
+      console.error("creating table", error);
+      return res.status(500).json({ message: "failed to create table" });
+    }
+    return res.status(200).json({ message: "table created" });
+  });
+};
 
-exports.createUfTable = (req,res)=>{
-    const createUf = 'CREATE TABLE urbanFarmer (uFid int AUTO_INCREMENT , NAME VARCHAR(50), id int ,FOREIGN KEY(id) references farmerOne(id), PRIMARY KEY(uFid))';
-    connection.query(createUf,(error,result)=>{
-        if(error){
-            console.error("creating table", error);
-            return res.status(500).json({ message: "failed to create table" });
-            
-        }
-        return res.status(200).json({ message: "table created" });
-    });
-}
-
+exports.createUfTable = (req, res) => {
+  const createUf =
+    "CREATE TABLE urbanFarmer (uFid int AUTO_INCREMENT , NAME VARCHAR(50), id int ,FOREIGN KEY(id) references farmerOne(id), PRIMARY KEY(uFid))";
+  connection.query(createUf, (error, result) => {
+    if (error) {
+      console.error("creating table", error);
+      return res.status(500).json({ message: "failed to create table" });
+    }
+    return res.status(200).json({ message: "table created" });
+  });
+};
+exports.registerFarmerOne = (req, res) => {
+  const sql = "INSERT INTO farmerOne (NAME) VALUES ('?')";
+  connection.query(sql, (error, result) => {
+    if (error) {
+      console.error("Error Occured", error);
+      return res.status(500).json({ message: "Failed to register farmer one" });
+    }
+    return res.status(200).json({ message: "Successfully created farmer one" });
+  });
+};
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/controller");
 
-router.get("/table",controller.createTable);
-router.get("/table2",controller.createUfTable);
-
+// router.get("/table",controller.createTable);
+// router.get("/table2", controller.createUfTable);
+router.post("/farmer-ones/", controller.registerFarmerOne);
 
 module.exports = router;
